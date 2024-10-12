7c10bd9a5f713053549122f15d8ab234c138bc05
NikaOmelchuk <120196531+NikaOmelchuk@users.noreply.github.com>
2024-10-12 22:01:37 +0300

    Add files via upload

---
+++ Threads/Lab14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            int[] b = new int[8];
+            int[] c = new int[5];
+
+            Console.WriteLine();
+            Console.WriteLine("Запис у файлi:");
+            Console.WriteLine();
+
+            Thread t1 = new Thread(() =>
+            {
+                SaveAsThread s = new SaveAsThread();
+                s.RandZap(a);
+                s.Save(a);
+            });
+
+            Thread t2 = new Thread(() =>
+            {
+                SaveAsThread s = new SaveAsThread();
+                s.RandZap(b);
+                s.Save(b);
+            });
+
+            Thread t3 = new Thread(() =>
+            {
+                SaveAsThread s = new SaveAsThread();
+                s.RandZap(c);
+                s.Save(c);
+            });
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Threads/Lab14/SaveAsThread.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Threads
+{
+    class SaveAsThread
+    {
+        private static object lo = new object();
+
+        public void Save(int[] a)
+        {
+            lock (lo)
+            {
+                using (StreamWriter writer = new StreamWriter("la14.txt", true))
+                {
+                    foreach (int i in a)
+                    {
+                        writer.Write(i + "  ");
+                        Console.Write(i + "  ");
+                    }
+                    writer.WriteLine();
+                    writer.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public void RandZap(int[] a)
+        {
+            Random r = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = r.Next(-50, 50);
+            }
+        }
+    }
+}
